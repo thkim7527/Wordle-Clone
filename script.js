481ab0bba5978099e8fbfd5d00250b2bc21a3c38
thkim7527 <thkim7527@gmail.com>
2022-02-23 12:39:06 +0900

    Fix Bug

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function handleKeyPress(event) {
 
 function next() {
     if (blockIndex === 4 && rowIndex === 5) { //On Game End
+        check();
         document.removeEventListener("keypress", handleKeyPress);
     } else if (blockIndex === 4) { //Go Next Line
         check();
